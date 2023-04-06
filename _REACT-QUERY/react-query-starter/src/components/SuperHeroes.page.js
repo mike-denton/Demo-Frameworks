98e6193326ac657aa65b9ac90d34f3d0e600fc6c
Mike Denton <mdenton@corrosionservice.com>
2023-04-06 15:12:00 -0400

    update demo frameworks

---
+++ _REACT-QUERY/react-query-starter/src/components/SuperHeroes.page.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const SuperHeroesPage = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(async () => {
+    // axios.get('http://localhost:4000/superheroes')
+    // .then(res => {
+    //   setData(res.data)
+    //   setIsLoading(false)
+    // })
+    debugger // BAAA
+    const res = await axios.get('http://localhost:4000/superheroes')
+    debugger //  BOOOO
+    setData(res.data)
+    setIsLoading(false)
+
+  }, [])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map(hero => {
+        return <div>{hero.name}</div>
+      })}
+    </>
+  )
+}
